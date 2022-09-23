2cddc7f3f927775ff35fb2569a9dc9b91ab81ee2
Julien-Butty <julienbutty@gmail.com>
2022-09-24 00:41:31 +0200

    Add HomepageController

---
+++ tests/Integration/Controller/HomepageControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomepageControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+    }
+}
